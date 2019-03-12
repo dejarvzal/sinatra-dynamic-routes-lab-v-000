62a3278d41fe0dc3a27cb00f7e7e2c14e7ddd5d9
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2019-03-12 14:09:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -22,8 +22,8 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-    @num1 = params[:number1]
-    @num2 = params[:number2]
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
 
     if params[:operation] = 'add'
       @answer = (num1+num2).to_s
